fad2d0ac79ef0bb17f3e421beccf62e67ecbe596
spiegelkarper <70937161+spiegelkarper@users.noreply.github.com>
2020-10-08 14:36:08 +0200

    Delete hamster download.ps1

--- hamster download.ps1
+++
@@ -1,9 +0,0 @@
-﻿#TEST Download
-$url="http://www.lesgalls.com/pics/bustybritain/gal8mq4j/"
-$x=wget -Uri $url
-$links=$x.Links|where href -like pic*.jpg|select -ExpandProperty href
-foreach ($l in $links)
-{
-$name=($l -split "/")[8]
-wget -Uri "$url$l" -OutFile "C:\Users\spieg\Desktop\nikkimay\$l"
-}
